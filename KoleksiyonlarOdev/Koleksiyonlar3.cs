b0b791d7fb892e59a10165aeeefbeaf20ca36f93
Bora Kaya <bora.587@hotmail.com>
2022-03-08 01:07:44 +0300

    Add project files.

---
+++ KoleksiyonlarOdev/Koleksiyonlar3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Koleksiyon3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList dizi = new ArrayList();
+            Console.Write("Lütfen bir cümle giriniz: ");
+            string metin = Console.ReadLine();
+            string sesli = "aeıioöuüAEIİOÖUÜ";
+            int sayac = 0;
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (sesli.Contains(metin[i]))
+                {
+                    sayac++;
+                    dizi.Add(metin[i]);
+                }
+
+            }
+
+            Console.WriteLine("Kelime içerisinde toplam " + sayac + " tane sesli harf var.");
+            Console.WriteLine("Bunlar Şöyledir.");
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
